ed8c79d41be9de0a61e23ed04bb57e07f37ea540
Hogward <jaegern23@gmail.com>
2024-03-05 18:27:51 -0600

    started adding styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,88 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: #333;
+  color: #fff;
+  padding: 1rem;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 2rem;
+`;
+
+const Nav = styled.nav`
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+  }
+
+  li {
+    margin-right: 1rem;
+  }
+`;
+
+const NavLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+  transition: color 0.3s ease;
+
+  &.active {
+    color: #ffd700; /* Change the active color as per your preference */
+  }
+
+  &:hover {
+    color: #ccc;
+  }
+`;
 
 function Header() {
   const location = useLocation();
 
   return (
-    <header>
-      <h1>Developer's Name</h1>
-      <nav>
+    <StyledHeader>
+      <Title>Jaeger Niles</Title>
+      <Nav>
         <ul>
           <li>
-            <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+            <NavLink
+              to="/"
+              className={location.pathname === "/" ? "active" : ""}
+            >
               About Me
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/portfolio"
               className={location.pathname === "/portfolio" ? "active" : ""}
             >
               Portfolio
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/contact"
               className={location.pathname === "/contact" ? "active" : ""}
             >
               Contact
-            </Link>
+            </NavLink>
           </li>
           <li>
-            <Link
+            <NavLink
               to="/resume"
               className={location.pathname === "/resume" ? "active" : ""}
             >
               Resume
-            </Link>
+            </NavLink>
           </li>
         </ul>
-      </nav>
-    </header>
+      </Nav>
+    </StyledHeader>
   );
 }
 
